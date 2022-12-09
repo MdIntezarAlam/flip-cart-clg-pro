6c3a3accaeeca5fe838732051e6561cf87a42071
FIRST_NAME LAST_NAME <mdintezara35@.com>
2022-12-09 11:22:48 +0530

    f

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import DefaultData from './default.js'
 import routers from './routes/routers.js'
+import path from 'path'
 
 const app = express()
 const PORT = process.env.PORT || 8000
@@ -20,39 +21,11 @@ app.use("/", routers)
 
 connectDatabase(URL)
 
-// Steps to Run Full Stack Application on Heroku
-//download heroku
-//cmd=> heroku
-//insert frontend folder in to backend folder 
-// change in backend file 
-// =>for port=> const PORT = process.env.PORT || 8000
-// for db => const URL = process.env.MONGODB_URI || `yaha par db ka path like ...`
-// //both are should be in index file
-
-//ye condition is only when backend file will run on production then client file wii run
-if (process.env.NODE_ENV === 'production') {
-    //1.before production we need to build the front-end file first
-    //2.backend mai server.js file hona neccessary
-    //3.package.json mai bhee server.js name neccessary
-    //4.front-end ko build karne ke leya backend ka package.json main script like
-      //  "scripts": {
-      //  "start": "node server.js",
-      //  "clint-install": "cd clint && npm install ",
-      //  "client-build": "cd client && npm run build",
-      //  "heroku-postbuild": "npm run client-install && npm run client-build"
-      //    },
-    // 6.and make a .gitignore file in main folder (backend file) 
-    // and add  jo jo nahi send karna hai frontend and backend files both it 
-        // node_modules => for backend
-        // package-lock.json => for backend
-        // .env => for backend
-        // client/node_modules => for frontend
-        // client/package-lock.json for frontend
-    //7.jis bhee file mai localhost use ho raha hai usko eemty kre=> const URL = "";
-    //8.find location and then build the client file
-    app.use(express.static('client/build')) //finding frontend location and building
-}
-
+// app.use(express.static('client/build')) //finding frontend location and building
+app.use(express.static(path.join(__dirname, './client/build')))
+app.get('*', function (req, res) {
+    res.sendFile(path.json(__dirname, './client/build/index.html'))
+});
 
 app.listen(PORT, () => {
     console.log(`server is listining on 🥰=>>>>>>>>>> ${PORT}`)
